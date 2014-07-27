bc295999bccd3e638da7ad7cd5932152f41bc7cf
bob walker <bob@randomness.org.uk>
2014-07-27 10:37:16 +0100

    Only create file if it doesnt already exist.
    If it doesnt exist add it to git and open an editor.
    If it does exist open it in an editor.

--- todo.sh
+++ todo.sh
@@ -3,6 +3,15 @@
 #adds things from previous file that werent done.
 DATE=`date +%Y-%m-%d`
 FILE=${DATE}.md
-echo "#### $DATE ####" > $FILE
-echo "" >> $FILE
-cat `ls -tr *md | grep -v "$DATE" |tail -1 `| grep "^-" | grep -v "~~" >> $FILE
+
+if [ -e $FILE ]
+then
+  echo "file already exists"
+  vi $FILE
+else
+  echo "#### $DATE ####" > $FILE
+  echo "" >> $FILE
+  cat `ls -tr *md | grep -v "$DATE" |tail -1 `| grep "^-" | grep -v "~~" >> $FILE
+  git add $FILE
+  vi $FILE
+fi
